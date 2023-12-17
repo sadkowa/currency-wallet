6fa3ae03a1abf0c8e5d8feb7a4bf20358525f16c
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 22:38:43 +0100

    Add ratesApiReducer in reducers

--- src/modules/reducers/index.js
+++ src/modules/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from "redux";
 
 import { reducer as localStorageReducer } from "../localStorage";
+import { reducer as ratesApiReducer } from '../exchangeRates'
+
 
 const reducers = combineReducers({
-    localStorage: localStorageReducer
+    localStorage: localStorageReducer,
+    ratesApi: ratesApiReducer
+
 })
 
 export default reducers

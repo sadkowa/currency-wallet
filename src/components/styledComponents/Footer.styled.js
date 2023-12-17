b78fb0ff7467ed1b9a1b6df9c57580fd4d1cc693
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:35:19 +0100

    Edit Footer.styled

--- src/components/styledComponents/Footer.styled.js
+++ src/components/styledComponents/Footer.styled.js
@@ -7,7 +7,20 @@ const StyledFooter = styled.footer`
     width: 100%;
     height: 80px;
     clip-path: polygon(0 0, 100% 26%, 100% 100%, 0% 100%);
+    background-color: ${({ theme }) => theme.colorsBG.dark.darkGrey};
+    font-size: ${({ theme }) => theme.fontSizes.small};
+    color: ${({ theme }) => theme.colorsText.light};
     z-index: 1;
+
+    @media ${({ theme }) => theme.media.tablet} {
+        height: 100px;
+        clip-path: polygon(0 0, 100% 26%, 100% 100%, 0% 100%);
+        font-size: ${({ theme }) => theme.fontSizes.medium};
+    }
+
+    @media ${({ theme }) => theme.media.desktop} {
+        font-size: ${({ theme }) => theme.fontSizes.large};
+    }
 `
 
 export default StyledFooter

ec4db536f7be5a07b524b996d1df2d7c49d60526
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:06:02 +0100

    Create Footer.styled.js

---
+++ src/components/styledComponents/Footer.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 80px;
+    clip-path: polygon(0 0, 100% 26%, 100% 100%, 0% 100%);
+    z-index: 1;
+`
+
+export default StyledFooter

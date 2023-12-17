aea1447e46bcddbc4784f3f7e9b52de3868df079
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 14:59:17 +0100

    Create ColorPicker

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import colorOptions from '../helpers/colorOptions'
+import { StyledColorPicker, StyledColorDiv } from './styledComponents/ColorPicker.styled'
+
+import { useSelector, useDispatch } from "react-redux";
+import { setActiveColor } from "../modules/localStorage";
+
+const ColorPicker = () => {
+
+    const { pickedColor } = useSelector(state => state.localStorage)
+    const dispatch = useDispatch()
+
+    const optionsRender = () => {
+        return colorOptions.map(({ id, color }) => {
+            return <StyledColorDiv
+                key={id}
+                option={color}
+                pickedcolor={pickedColor}
+                onClick={() => dispatch(setActiveColor(color))} />
+        })
+    }
+
+    return (
+        <StyledColorPicker>
+            {optionsRender()}
+        </StyledColorPicker>
+    )
+}
+
+export default ColorPicker
+

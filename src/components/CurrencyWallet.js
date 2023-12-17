b17e13676d0ae08f9d619e80d6cefa629e590ee2
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:05:17 +0100

    Create Footer

--- src/components/CurrencyWallet.js
+++ src/components/CurrencyWallet.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 const CurrencyWallet = () => {
     return(
             <Header />
-            // <Footer />
+            <Footer />
     )
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StyledFooter from "./styledComponents/Footer.styled";
+
+const Footer = () => {
+    return (
+        <StyledFooter>
+            <p>&copy; All rights reserved</p>
+        </StyledFooter>
+    )
+}
+
+export default Footer

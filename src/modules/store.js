93ba269e0aa40b30b12160072547a1ba5bdf8a2e
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 22:47:30 +0100

    Configure store

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
-
+import {thunk} from 'redux-thunk'
 import reducers from "./reducers";
 
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { loadFromLocalStorage, saveToLocalStorage } from "./localStorage";
 
-const store = createStore(reducers, loadFromLocalStorage(), applyMiddleware());
+const store = createStore(reducers, loadFromLocalStorage(), composeWithDevTools(applyMiddleware(thunk)));
 
 store.subscribe(() => saveToLocalStorage(store.getState()));
 

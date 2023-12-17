7ff35222f885d96bb3a1be61720fac10d5bdc214
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 22:36:02 +0100

    Create exchangeRates.actions.js

---
+++ src/modules/exchangeRates/exchangeRates.actions.js
@@ -0,0 +1,37 @@
+import { types } from "."
+import ExchangeRatesApi from "./exchangeRatesApi"
+
+export const getRatesData = (endpoint) => (dispatch) => {
+    const api = new ExchangeRatesApi()
+
+    api.getData(endpoint)
+        .then(data => {
+            if (endpoint.includes('latest')) {
+                dispatch(setLatestRatesData(data.rates))
+            } 
+            if(endpoint.includes('historical')) {
+                dispatch(setHistoricalRatesData(data.rates))
+            }
+        })
+        .catch(err => console.log(err))
+}
+
+export const setLatestRatesData = rates => {
+    return {
+        type: types.LATESTRATESDATA_SET,
+        payload: { rates }
+    }
+}
+export const setHistoricalRatesData = rates => {
+    return {
+        type: types.HISTORICALRATESDATA_SET,
+        payload: { rates }
+    }
+}
+
+export const clearHistoricalRatesData = () => {
+    return {
+        type: types.HISTORICALRATESDATA_CLEAR,
+        
+    }
+}

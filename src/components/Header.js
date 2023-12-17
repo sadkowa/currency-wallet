2270ece37d209ee5abdacb45efeaf3b75e3ea06b
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 12:59:53 +0100

    Create Header.styled.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { StyledHeader, StyledMainHeading } from "./styledComponents/Header.styled";
 
 const Header = () => {
 
---
+++ src/components/styledComponents/Header.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 80px;
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 80%);
+    background-color: ${({ theme, color }) => theme.colorsBG.dark[color]};
+
+    @media ${({ theme }) => theme.media.tablet} {
+        height: 100px;
+        clip-path: polygon(0 0, 100% 0, 100% 100%, 0 70%);
+    }
+
+    @media ${({ theme }) => theme.media.desktop} {
+        height: 120px;
+    }
+`
+
+const StyledMainHeading = styled.h1`
+    width:  70%;
+    color: ${({ theme }) => theme.colorsText.light};
+    font-size: ${({ theme }) => theme.fontSizes.xlarge};
+    text-transform: uppercase;
+    text-align: center;
+    letter-spacing: 3px;
+
+    @media ${({ theme }) => theme.media.tablet} {
+        font-size: ${({ theme }) => theme.fontSizes.xxlarge}
+       }
+
+    @media ${({ theme }) => theme.media.desktop} {
+        font-size: ${({ theme }) => theme.fontSizes.xxxlarge}
+    }
+`
+
+export {StyledHeader, StyledMainHeading}

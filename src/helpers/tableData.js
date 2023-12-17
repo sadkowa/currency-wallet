0f4bffc0c814128b51fb1de9e2fe8453b6e83b1e
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 22:08:43 +0100

    Create tableData helper

---
+++ src/helpers/tableData.js
@@ -0,0 +1,13 @@
+export const tableHeadings = [
+    'Currency',
+    'Amount',
+    'Date of purchase',
+    'Purchase rate',
+    'Current rate',
+    'Current value',
+    'Profit/ loss'
+]
+
+export const noDataMessage = 'Sorry, there is no data to display. Please fill out the form above to access the summary information.'
+
+export const popUpMessage = 'Are you sure you want to delete this item?'

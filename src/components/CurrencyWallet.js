26ee47af44a05d323412f243e1818287025ada20
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:08:22 +0100

    Create StyledSection

--- src/components/CurrencyWallet.js
+++ src/components/CurrencyWallet.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
+import { StyledSection } from "./styledComponents/CurrencyWallet.styled";
 
 const CurrencyWallet = () => {
     return(
+        <StyledSection>
             <Header />
             <Footer />
+        </StyledSection>
     )
 }
 
---
+++ src/components/styledComponents/CurrencyWallet.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const StyledSection = styled.section`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`
+
+export { StyledSection }

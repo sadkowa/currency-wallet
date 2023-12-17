748c2d00911cfeb3635ef0d350f0deac27a99336
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:33:23 +0100

    Edit Header.styled

--- src/components/styledComponents/Header.styled.js
+++ src/components/styledComponents/Header.styled.js
@@ -5,14 +5,34 @@ const StyledHeader = styled.header`
     justify-content: center;
     align-items: center;
     height: 80px;
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 80%); 
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 80%);
+    background-color: blue;
+
+    @media ${({ theme }) => theme.media.tablet} {
+        height: 100px;
+        clip-path: polygon(0 0, 100% 0, 100% 100%, 0 70%);
+    }
+
+    @media ${({ theme }) => theme.media.desktop} {
+        height: 120px;
+    }
 `
 
 const StyledMainHeading = styled.h1`
-    width:  70%; 
+    width:  70%;
+    color: ${({ theme }) => theme.colorsText.light};
+    font-size: ${({ theme }) => theme.fontSizes.xlarge};
     text-transform: uppercase;
     text-align: center;
-    letter-spacing: 3px;  
+    letter-spacing: 3px;
+
+    @media ${({ theme }) => theme.media.tablet} {
+        font-size: ${({ theme }) => theme.fontSizes.xxlarge}
+       }
+
+    @media ${({ theme }) => theme.media.desktop} {
+        font-size: ${({ theme }) => theme.fontSizes.xxxlarge}
+    }
 `
 
 export {StyledHeader, StyledMainHeading}

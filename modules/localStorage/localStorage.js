5191c21483bc6dee11ed31c3a4db6ef3b694e46d
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 14:30:04 +0100

    Create localStorage.js

---
+++ modules/localStorage/localStorage.js
@@ -0,0 +1,19 @@
+export function saveToLocalStorage(state) {
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+        console.warn(e);
+    }
+}
+
+export function loadFromLocalStorage() {
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+        if (serialisedState === null) return undefined;
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+}

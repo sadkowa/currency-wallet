98920dfba6757d3da1a7be1555bf2d328c03af80
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:29:55 +0100

    Add ThemeProvider in index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider } from 'styled-components';
+import themeSettings from './Theme/theme';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeSettings}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 

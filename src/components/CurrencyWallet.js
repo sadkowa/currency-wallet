9db943a206e167fa35aa3be5250160334e8e0176
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:23:12 +0100

    Create Table

--- src/components/CurrencyWallet.js
+++ src/components/CurrencyWallet.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import WalletForm from "./WalletForm";
+import Table from "./Table";
 import { StyledSection, Wrapper } from "./styledComponents/CurrencyWallet.styled";
 
 const CurrencyWallet = () => {
@@ -10,6 +11,7 @@ const CurrencyWallet = () => {
             <Header />
             <Wrapper>
                 <WalletForm />
+                <Table />
             </Wrapper>
             <Footer />
         </StyledSection>

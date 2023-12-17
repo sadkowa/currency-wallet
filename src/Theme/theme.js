2ae129002897127ef58c74bf464fab99b93ccb57
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-17 13:27:44 +0100

    Create Theme

---
+++ src/Theme/theme.js
@@ -0,0 +1,55 @@
+const themeSettings = {
+    colorsBG: {
+        light: {
+            grey: '#d4e9f9',
+            purple: '#f2e6f7',
+            blue: '#d1f5ff',
+        },
+        dark: {
+            grey: '#8395a7',
+            purple: '#a73ed1',
+            blue: '#4CB5F9',
+            darkGrey: '#5f6c77',
+
+        },
+        darkHover: {
+            grey: '#6f7f8e',
+            purple: '#941ac9',
+            blue: '#0CB0F5',
+        }
+    },
+    colorsText: {
+        light: '#fff',
+        dark: '#5c6a77',
+        darkText: '#595d60',
+    },
+    paddings: {
+        xxsmall: '4px',
+        xsmall: '7px',
+        small: '10px',
+        medium: '20px',
+        large: '30px',
+        xlarge: '40px',
+        xxlarge: '60px',
+    },
+    borderRadius: {
+        small: '5px',
+        medium: '10px',
+        large: '15px',
+    },
+    fontSizes: {
+        xsmall: '0.8rem',
+        small: '1.0rem',
+        medium: '1.3rem',
+        large: '1.5rem',
+        xlarge: '2.0rem',
+        xxlarge: '2.5rem',
+        xxxlarge: '3.0rem',
+    },
+    media: {
+        tablet: 'screen and (min-width: 600px)',
+        desktop: 'screen and (min-width: 1000px)',
+    }
+}
+
+export default themeSettings

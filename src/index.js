fe4ac6e39d953da1caf58fc8ae3e939cc884bea4
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-18 11:21:24 +0100

    Check ApiKey

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ root.render(
     <ThemeProvider theme={themeSettings}>
       <ResetStyle />
       <GlobalStyle />
+        <div>{process.env.REACT_APP_API_KEY}</div>
+
       <App />
     </ThemeProvider>
     </Provider>

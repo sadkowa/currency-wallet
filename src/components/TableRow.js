4dd451a67e6664e70b1d43573c3191ced911273a
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2023-12-18 12:17:03 +0100

    Count values in table

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,17 +1,37 @@
 import React, { useEffect } from "react";
 import { StyledRow, StyledCell, StyledButton } from "./styledComponents/TableRow.styled";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getRatesData } from "../modules/exchangeRates";
 
 
 const TableRow = ({ item, isPopupActive, setIsPopupActive, setIdToDelete }) => {
     const dispatch = useDispatch()
 
-    const { select, amount, date, id } = item
+    const { latestRates } = useSelector(state => state.ratesApi)
+
+    const { select, amount, date, rate, id } = item
+
+    const historicalRate = Number(rate).toFixed(2)
+    const currentRate = getCurrentRate(select).toFixed(2)
+    const currentAmount = countValue(amount, currentRate)
+    const purchaseAmount = countValue(amount, rate)
+    const divideResult = (currentAmount - purchaseAmount).toFixed(2)
+    const divideResultPercentages = Math.round(divideResult / purchaseAmount * 100)
 
     useEffect(() => {
         dispatch(getRatesData('latest'))
     }, [])
+
+    function getCurrentRate(select) {
+        const pln = latestRates['PLN']
+        const plnRate = pln / latestRates[select]
+
+        return plnRate
+    }
+
+    function countValue(amount, rate) {
+        return (amount * rate).toFixed(2)
+    }
 
     const handleClick = id => {
         // setIsPopupActive(!isPopupActive)
@@ -23,10 +43,10 @@ const TableRow = ({ item, isPopupActive, setIsPopupActive, setIdToDelete }) => {
             <StyledCell >{select}</StyledCell>
             <StyledCell >{amount}</StyledCell>
             <StyledCell >{date}</StyledCell>
-            <StyledCell >{'historicalRate'}</StyledCell>
-            <StyledCell >{'currentRate'}</StyledCell>
-            <StyledCell >{'currentAmount'}</StyledCell>
-            <StyledCell >{'divideResult'}''</StyledCell>
+            <StyledCell >{historicalRate}</StyledCell>
+            <StyledCell >{currentRate}</StyledCell>
+            <StyledCell >{currentAmount}</StyledCell>
+            <StyledCell >{divideResult}''</StyledCell>
             <StyledCell >
                 <StyledButton onClick={() => handleClick(id)}>+</StyledButton>
             </StyledCell>
